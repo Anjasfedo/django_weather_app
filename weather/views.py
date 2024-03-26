b6e0ef1fef828821123d92f05d63ce852d0c3af3
AnjasFedo <fedoaf98@gmail.com>
2024-03-26 17:01:26 +0700

    Layout & Implement HTMX

--- weather/views.py
+++ weather/views.py
@@ -9,6 +9,7 @@ def index(request):
     if request.method == 'POST':
         city = request.POST['city']
         encoded_city = quote(city)
+        
         try:
             res = urllib.request.urlopen(f'http://api.openweathermap.org/data/2.5/weather?q={encoded_city}&appid=cb771e45ac79a4e8e2205c0ce66ff633').read()
             json_data = json.loads(res)
@@ -20,19 +21,27 @@ def index(request):
                 "pressure": str(json_data['main']['pressure']),
                 "humidity": str(json_data['main']['humidity']),
             }
+            
         except urllib.error.HTTPError as e:
             # Handle HTTP errors
             error_message = f"HTTP Error: {e.code} - {e.reason}"
             data = {'error': error_message}
+            
         except urllib.error.URLError as e:
             # Handle URL errors
             error_message = f"URL Error: {e.reason}"
             data = {'error': error_message}
+            
         except json.JSONDecodeError as e:
             # Handle JSON decoding errors
             error_message = f"JSON Decode Error: {str(e)}"
             data = {'error': error_message}
-    else:
-        city = ''
-        data = {}
-    return render(request, 'index.html', {'city': city, 'data': data})
+            
+        context = {
+            'city': city, 
+            'data': data
+            }
+    
+        return render(request, 'htmx/result.html', context)
+    
+    return render(request, 'index.html')
